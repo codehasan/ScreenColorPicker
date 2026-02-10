3dcf7699044a390ee42c5a04051f48d0e2808e05
Ratul Hasan <ratul.hasan.rahat.bd@gmail.com>
2026-02-11 00:34:39 +0600

    add context extension

---
+++ app/src/main/java/io/github/codehasan/colorpicker/extensions/ContextExtension.kt
@@ -0,0 +1,11 @@
+package io.github.codehasan.colorpicker.extensions
+
+import android.content.Context
+import android.util.TypedValue
+
+fun Context.dp2px(dp: Float): Int =
+    TypedValue.applyDimension(
+        TypedValue.COMPLEX_UNIT_DIP,
+        dp,
+        resources.displayMetrics
+    ).toInt()
